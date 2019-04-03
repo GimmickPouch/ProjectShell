9d83573672415862a813f477fed39eae2aaabfc1
GimmickPouch <luke.alexander.perry@gmail.com>
2019-04-03 22:05:57 +0100

    Added TankPlayerStart for future editing

---
+++ ProjectShell/Source/ProjectShell/PlayerStarts/TankPlayerStart.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "TankPlayerStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSHELL_API ATankPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
